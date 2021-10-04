1b5c8c10ec2284f86543268c50693f6b89882bbb
Marado-Programer <marado.pen.eng@gmail.com>
2021-10-05 00:57:21 +0100

    add autoresize patch to dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int attachdirection = 0;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static const int attachdirection = 4;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
