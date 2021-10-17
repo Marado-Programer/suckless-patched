3592f2cf0f976c62e527deb6f1365849604c8e85
Marado-Programer <marado.pen.eng@gmail.com>
2021-10-17 19:52:04 +0100

    add fakefullscreen patch to dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -49,7 +49,7 @@ static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int decorhints  = 1;    /* 1 means respect decoration hints */
 static const int attachdirection = 4;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
